3957adfb3f78786db0dbf92c27e718012e0e2dae
sumesh-tg <sumesh.tg@hotmail.com>
2023-01-19 22:10:55 +0530

    Routing check bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/dashboard/body/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DisplayComponent } from './components/display/display.component';
-import { PrintComponent } from './components/samples/print/print.component';
+import { TokenComponent } from './components/token/token.component';
 
 const routes: Routes = [
-
-  {path:"",redirectTo:"/dashboard/token",pathMatch:"full"},
+  {path:"",redirectTo:"/dashboard",pathMatch:"full"},
   {path:"dashboard" , component :DashboardComponent,children:[
     {path:"",redirectTo:"/dashboard/token",pathMatch:"full"},
     {path:'display',component:DisplayComponent},
-    {path:'token',component:DisplayComponent}
+    {path:'token',component:TokenComponent},
+    {path:'**',component:DashboardComponent}
   ]} 
-  // {path:"",component:PrintComponent},
-  // {path:"dashboard" , component :DashboardComponent,children:[
-    // {path:"",redirectTo:"/dashboard/home",pathMatch:"full"},
-    // {path:'blog',component:AocBlogComponent},
-    // {path:'home',component:HomeComponent},
-    // {path:'contact-us',component:ContactUsComponent},
-    // {path:'add-vendor',component:AddVendorComponent},
-    // {path:'add-post',component:AddPostComponent},
-    // {path:'blog-post',component:BlogPostComponent},
-    // {path:'delete-post',component:DeletePostComponent},
-    // {path:'preview',component:BlogPostComponent},
-    // {path:'add-stock',component:AddStockDetailsComponent},
-    // {path:'add-quality',component:AddQualityReportComponent},
-    // {path:'aboutus',component:AocBlogComponent},
-    // {path:'stock-details',component:StockDetailsComponent},
-    // {path:'quality-report',component:QualityReportComponent},
-    // {path:'**',component:DashboardComponent}
-  // ]} 
 ];
 
 @NgModule({
